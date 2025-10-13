d7a00a2abcaa68f11d8c1be37cdda7bbc52a3da6
wahyudi.indra <wahyudiindra79@gmail.com>
2025-10-13 11:51:34 +0700

    feat(transaction): handling race condition

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -36,38 +36,45 @@ export class TransactionsService extends BaseRepository {
         let totalAmount = 0;
         let items: Prisma.TransactionItemCreateManyTransactionInput[] = [];
 
-        const carts = await this.prisma.cart.findMany({
-            where: { id: { in: cartIds }, userId: user.id },
-            include: { book: true },
-        });
-        if (carts.length !== cartIds.length) throw new BadRequestException('Invalid cart ids');
+        return this.prisma.$transaction(
+            async (prisma) => {
+                const carts = await prisma.cart.findMany({
+                    where: { id: { in: cartIds }, userId: user.id },
+                    include: { book: true },
+                });
 
-        return this.prisma.$transaction(async (prisma) => {
-            await Promise.all(
-                carts.map(async ({ book, ...cart }) => {
-                    if (book.stock < cart.qty) {
-                        throw new BadRequestException(`The quantity of '${book.title}' exceeds the available stock`);
-                    }
+                if (carts.length !== cartIds.length) throw new BadRequestException('Invalid cart ids');
+                await Promise.all(
+                    carts.map(async ({ book, ...cart }) => {
+                        const temp = await prisma.book.update({
+                            where: { id: cart.bookId },
+                            data: { stock: { decrement: cart.qty } },
+                        });
+                        if (temp.stock < 0) {
+                            throw new BadRequestException(
+                                `The quantity of '${book.title}' exceeds the available stock`,
+                            );
+                        }
 
-                    await prisma.book.update({ where: { id: cart.bookId }, data: { stock: { decrement: cart.qty } } });
+                        totalAmount += cart.qty * Number(book.price);
 
-                    totalAmount += cart.qty * Number(book.price);
+                        items.push({ bookId: cart.bookId, price: book.price, quantity: cart.qty });
+                    }),
+                );
 
-                    items.push({ bookId: cart.bookId, price: book.price, quantity: cart.qty });
-                }),
-            );
+                await prisma.cart.deleteMany({ where: { id: { in: cartIds } } });
 
-            await prisma.cart.deleteMany({ where: { id: { in: cartIds } } });
-
-            return prisma.transaction.create({
-                data: {
-                    totalAmount,
-                    userId: user.id,
-                    items: { createMany: { data: items } },
-                    status: TransactionStatus.PENDING,
-                },
-            });
-        });
+                return prisma.transaction.create({
+                    data: {
+                        totalAmount,
+                        userId: user.id,
+                        items: { createMany: { data: items } },
+                        status: TransactionStatus.PENDING,
+                    },
+                });
+            },
+            { isolationLevel: 'Serializable' },
+        );
     }
 
     async callback(callbackData: CallbackTransactionDto) {
