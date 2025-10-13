7f54163cd4532649546bd00f4591557a09d94f9e
wahyudi.indra <wahyudiindra79@gmail.com>
2025-10-13 08:21:59 +0700

    feat(schema): set migration for create cart table

---
+++ prisma/migrations/20251013012141_create_cart_table/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "carts" (
+    "id" TEXT NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "book_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "carts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "carts_user_id_book_id_key" ON "carts"("user_id", "book_id");
+
+-- AddForeignKey
+ALTER TABLE "carts" ADD CONSTRAINT "carts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "carts" ADD CONSTRAINT "carts_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
